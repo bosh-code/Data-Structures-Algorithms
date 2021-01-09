0d8d9178517f8443822482c307a247103369397f
Ryan Bosher <ryan.julian.bosher@icloud.com>
2021-01-09 14:52:07 +1300

    updated BinSort.java

--- src/binSort/BinSort.java
+++ src/binSort/BinSort.java
@@ -14,17 +14,18 @@ public final class BinSort {
 	public static void main(String... args) {
 		SecureRandom rand = new SecureRandom();
 		int[] list = new int[ARRAY_LENGTH];
+		final int maxValue = 100;
+		int[] flags = new int[maxValue + 1];
+		int flagNo = 0;
+		
 		for (int i = 0; i < ARRAY_LENGTH; i++) {
 			list[i] = rand.nextInt(ARRAY_LENGTH << 2);
 		}
-		final int maxValue = 100;
-		int[] flags = new int[maxValue + 1];
 		
 		for (int value : list) {
 			flags[value]++;
 		}
 		
-		int flagNo = 0;
 		for (int i = 0; i < list.length; i++) {
 			while (flagNo < flags.length && !(flags[flagNo] >= 1)) {
 				flagNo++;
